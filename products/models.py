a9183dcd51a84f5c4e99f1f7beec076d65e38d88
yug237 <65022409+yug237@users.noreply.github.com>
2021-05-31 21:39:00 +0530

    y

--- products/models.py
+++ products/models.py
@@ -7,6 +7,7 @@ from django.contrib.auth.models import User
 class compP(models.Model):
 	Pcomp = models.CharField(max_length=200)
 	Pname = models.CharField(max_length=200)
+	Pcategory = models.CharField(max_length=200, default='Random')
 	Pdesc = models.TextField()
 	Pprice = models.IntegerField(default=0)
 	Pimage = models.ImageField(upload_to='Product_pics', default='/media/Product_pics/default.jpg')
